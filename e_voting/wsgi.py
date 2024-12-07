3aa4fcdaccb500a90ee0b959b80e21a1132212f4
simon9001 <simongatungo300@gmail.com>
2024-12-07 08:59:39 +0300

    second

--- e_voting/wsgi.py
+++ e_voting/wsgi.py
@@ -7,10 +7,30 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_voting.settings')
+
+#application = get_wsgi_application()
+"""
+WSGI config for Systemproject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_voting.settings')
+settings_module = 'azure-project.deployment'if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
+
+
